acd891630b4f0295f21b38f7b800403a018d8f64
Michael DeFreitas <mike@applauze.com>
2016-02-03 13:24:37 -0800

    Bill methods to invoke Approvals APIs.

--- lib/rubill/bill.rb
+++ lib/rubill/bill.rb
@@ -4,6 +4,16 @@ module Rubill
       SentPayment.create(opts)
     end
 
+    def self.set_approvers(bill_id, approver_user_id_array)
+      Rubill::Query.execute('/SetApprovers.json', entity: 'Bill',
+                            objectId: bill_id,
+                            approvers: approver_user_id_array)
+    end
+
+    def self.approve_bill(bill_id)
+      Rubill::Query.execute('/Approve.json', entity: 'Bill', objectId: bill_id)
+    end
+
     def self.remote_class_name
       "Bill"
     end
